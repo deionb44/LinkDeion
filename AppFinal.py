bc6542e58009dff481c8f99cdbe7354f5ece9628
deionb44 <120497721+deionb44@users.noreply.github.com>
2022-12-13 13:58:08 -0500

    Update AppFinal.py

--- AppFinal.py
+++ AppFinal.py
@@ -54,7 +54,7 @@ print(y,X)
 # In[5]:
 
 
-
+from sklearn.model_selection import train_test_split 
 X_train, X_test, y_train, y_test = train_test_split(X, 
                                                     y, 
                                                     stratify=y,       # same number of target in training & test set 
